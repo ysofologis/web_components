5059dcd3f8d7710dbdab03df22ce08022f614672
freesrc <freesrc@localhost.localdomain>
2014-03-16 00:37:43 +0200

    making it more java-script "modular"

--- CollectionViewer/public_html/models/collectionview.js
+++ CollectionViewer/public_html/models/collectionview.js
@@ -4,96 +4,116 @@
  * and open the template in the editor.
  */
 
+var mylib = mylib || {};
+mylib.utils = mylib.utils || {};
 
-var app_defaults = {};
+mylib.utils.collections = (function(window, $) {
 
-Object.defineProperty(app_defaults, "zero_opacity", {
-    enumerable: false,
-    configurable: false,
-    writable: false,
-    value: 0.001
-});
+    var app_defaults = {};
 
-Object.defineProperty(app_defaults, "full_opacity", {
-    enumerable: false,
-    configurable: false,
-    writable: false,
-    value: 1.00
-});
+    Object.defineProperty(app_defaults, "zero_opacity", {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: 0.001
+    });
 
-Object.defineProperty(app_defaults, "show_hide_delay", {
-    enumerable: false,
-    configurable: false,
-    writable: false,
-    value: 450
-});
+    Object.defineProperty(app_defaults, "full_opacity", {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: 1.00
+    });
 
-Object.defineProperty(app_defaults, "resize_delay", {
-    enumerable: false,
-    configurable: false,
-    writable: false,
-    value: 100
-});
+    Object.defineProperty(app_defaults, "show_hide_delay", {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: 450
+    });
 
-Object.defineProperty(app_defaults, "item_visible_class", {
-    enumerable: false,
-    configurable: false,
-    writable: false,
-    value: "item-visible"
-});
+    Object.defineProperty(app_defaults, "resize_delay", {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: 100
+    });
 
-Object.defineProperty(app_defaults, "item_hidden_class", {
-    enumerable: false,
-    configurable: false,
-    writable: false,
-    value: "item-hidden"
-});
+    Object.defineProperty(app_defaults, "item_visible_class", {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: "item-visible"
+    });
 
-function CollectionViewer(pageContainerId, collectionContainerId, items, collectionName) {
+    Object.defineProperty(app_defaults, "item_hidden_class", {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: "item-hidden"
+    });
+
+    function createObservable(initialValue) {
+        return ko.observable(initialValue);
+    }
+
+    function createObservableArray(initialData) {
+        return ko.observableArray(initialData);
+    }
+    
+    function createComputed(func, binder) {
+        return ko.computed(func, binder);
+    }
 
     function ItemContainer(id, model, idPrefix) {
         var self = this;
 
         self.itemId = idPrefix + "-item-" + id;
         self.model = model;
-        
+
         var _isVisible = true;
 
         //self.opacity = ko.observable(app_defaults.zero_opacity);
-        
-        self.isVisible = function(){
+
+        self.isVisible = function() {
             return _isVisible;
         };
-        
+
         self.show = function() {
             $("#" + self.itemId).removeClass(app_defaults.item_hidden_class).addClass(app_defaults.item_visible_class);
             _isVisible = true;
         };
-        
+
         self.hide = function() {
             $("#" + self.itemId).removeClass(app_defaults.item_visible_class).addClass(app_defaults.item_hidden_class);
             _isVisible = false;
         };
     }
-    
+
+    /*
+     * visible items range
+     * @param {type} from
+     * @param {type} to
+     * @returns {_L10.ItemsRange}
+     */
     function ItemsRange(from, to) {
-        this.from = ko.observable(from);
-        this.to = ko.observable(to);
+        this.from = createObservable(from);
+        this.to = createObservable(to);
     }
 
-    function ItemsPage(items, idPrefix) {
+    /*
+     * How many items on page
+     * @param {type} items
+     * @param {type} idPrefix
+     * @returns {undefined}
+     */
+    function ItemsPage(items) {
 
         var self = this;
 
-        self.availableItems = [];
-        self.pageIndex = ko.observable(1);
-        self.visibleRange = new ItemsRange(0, items.length-1);
-
-        if (items) {
-            _.each(items, function(i) {
-                self.availableItems.push(new ItemContainer(self.availableItems.length + 1, i, idPrefix));
-            });
-        }
+        self.availableItems = items;
+        self.pageIndex = createObservable(1);
+        self.visibleRange = new ItemsRange(0, items.length - 1);
 
         self.showItems = function(containerHeight) {
 
@@ -103,7 +123,7 @@ function CollectionViewer(pageContainerId, collectionContainerId, items, collect
             for (var ix = self.visibleRange.from(); ix < self.availableItems.length; ix++) {
 
                 lastIndex = 0;
-                
+
                 var currentItem = self.availableItems[ix];
 
                 function showCurrent(anItem) {
@@ -117,12 +137,14 @@ function CollectionViewer(pageContainerId, collectionContainerId, items, collect
                             anItem.show();
                         }, 150);
 
+                        self.visibleRange.to(ix);
+
                         return true;
 
                     } else {
                         console.log("item exceeds container height.");
-                        
-                        if ( ! anItem.isVisible() ) {
+
+                        if (!anItem.isVisible()) {
                             return false;
                         } else {
                             anItem.hide();
@@ -135,150 +157,139 @@ function CollectionViewer(pageContainerId, collectionContainerId, items, collect
                     break;
                 }
             }
-            
+
             return lastIndex;
         };
     }
 
-    var self = this;
 
-    // var _collectionName = collectionName;
-    var _pageContainerId = pageContainerId;
-    var _collectionContainerId = collectionContainerId;
+    function DynamicPager(pageContainerId, collectionContainerId, items, collectionName) {
 
-    self.actualHeight = ko.observable("100%");
-    self.allItems = ko.observable(items);
-    self.currentPage = new ItemsPage(items, collectionName);
-    self.lastVisibleIndex = ko.observable(0);
-    
-    self.itemsCount = ko.computed(function () {
-       var r = self.allItems().length;
-       return r;
-    }, self);
-    
-    
-    self.hasPrev = ko.computed(function () {
-        return self.currentPage.pageIndex() > 1;
-    }, self);
-    
-    self.hasNext = ko.computed(function () {
-        return self.lastVisibleIndex() < (self.itemsCount() - 1);
-    }, self);
+        var self = this;
 
-    console.log("current page loaded");
+        // var _collectionName = collectionName;
+        var _pageContainerId = pageContainerId;
+        var _collectionContainerId = collectionContainerId;
 
-    /*
-     self.actualHeight.subscribe(function(newValue) {
-     console.log(newValue);
-     });
-     */
+        self.actualHeight = createObservable("100%");
+        self.availableItems = [];
+        
+        if (items) {
+            _.each(items, function(i) {
+                self.availableItems.push(new ItemContainer(self.availableItems.length + 1, i, collectionName));
+            });
+        }
+        
+        self.currentPage = new ItemsPage(self.availableItems);
 
-    function syncHeight() {
-        var h = $(_pageContainerId).height();
-        self.actualHeight(h + "px");
+        self.itemsCount = createComputed(function() {
+            var r = self.availableItems.length;
+            return r;
+        }, self);
 
-        return h;
-    }
-    ;
+        self.fromVisible = createComputed(function() {
+            return self.currentPage.visibleRange.from() + 1;
+        }, self);
 
-    self.increaseBodySize = function() {
-        var h = $('body').height();
-        h += h * 0.10;
-        $('body').height(h);
-    };
+        self.toVisible = createComputed(function() {
+            return self.currentPage.visibleRange.to() + 1;
+        }, self);
 
-    function _onResize(e) {
-        console.log(e);
-        setTimeout(function() {
-            syncHeight();
-            self.show(app_defaults.show_hide_delay);
-        },app_defaults.resize_delay);
-    }
-    
+        console.log("current page loaded");
 
-    var lastScroll = 0;
+        /*
+         self.actualHeight.subscribe(function(newValue) {
+         console.log(newValue);
+         });
+         */
 
-    function _onScroll(e) {
-        console.log(e);
-        var currentScroll = $(window).scrollTop();
-        // is this 'scroll-down' ?
-        if (currentScroll > lastScroll) {
-            setTimeout(function () {
+        function syncHeight() {
+            var h = $(_pageContainerId).height();
+            self.actualHeight(h + "px");
+
+            return h;
+        }
+        ;
+
+        /**
+         * only for testing
+         * @returns {undefined}
+         */
+        self.increaseBodySize = function() {
+            var h = $('body').height();
+            h += h * 0.10;
+            $('body').height(h);
+        };
+
+        function onResize(e) {
+            console.log(e);
+            setTimeout(function() {
                 syncHeight();
                 self.show(app_defaults.show_hide_delay);
             }, app_defaults.resize_delay);
         }
-        
-        lastScroll = currentScroll;
-    };
 
-    $(document).ready(function() {
 
-        $(window).resize(_onResize);
-        $(window).on('scroll', _onScroll);
+        var lastScroll = 0;
 
-    });
+        function onScroll(e) {
+            console.log(e);
+            var currentScroll = $(window).scrollTop();
+            // is this 'scroll-down' ?
+            if (currentScroll > lastScroll) {
+                setTimeout(function() {
+                    syncHeight();
+                    self.show(app_defaults.show_hide_delay);
+                }, app_defaults.resize_delay);
+            }
 
-    // window.document.body.onresize = self._onResize;
-    var _lastShow = new Date();
-
-    self.show = function(delay) {
-        
-        var nowTime = new Date();
-        var duration = nowTime - _lastShow;
-        
-        if ( duration >= delay ) {
-            setTimeout(function() {
-                var lastIndex = self.currentPage.showItems($(_collectionContainerId).height());
-                self.lastVisibleIndex(lastIndex);
-            }, delay);
-            
-            _lastShow = nowTime;
+            lastScroll = currentScroll;
         }
-    };
-    
-    self.showNext = function() {
-        console.log("<showNext>");
-    };
-    
-    self.showPrev = function() {
-        console.log("<showPrev>");
-        
-    };
+        ;
 
-}
+        $(document).ready(function() {
 
-function createItem(header, text, lines) {
-    var item = {header: header, text: ""};
+            $(window).resize(onResize);
+            $(window).on('scroll', onScroll);
 
-    for (var ix = 0; ix < lines; ix++) {
-        item.text += text + "\n";
+        });
+
+        // window.document.body.onresize = self._onResize;
+        var _lastShow = new Date();
+
+        self.show = function(delay) {
+
+            var actualDelay = delay || 0;
+
+            var nowTime = new Date();
+            var duration = nowTime - _lastShow;
+
+            if (duration >= actualDelay) {
+                setTimeout(function() {
+                    self.currentPage.showItems($(_collectionContainerId).height());
+                }, actualDelay);
+
+                _lastShow = nowTime;
+            }
+        };
+
+        self.showNext = function() {
+            console.log("<showNext>");
+        };
+
+        self.showPrev = function() {
+            console.log("<showPrev>");
+
+        };
+
     }
 
-    return item;
-}
+    var module = {
+        CollectionViewer: DynamicPager,
+        Defaults: app_defaults
+    };
 
-var collectionItems = [
-    createItem('01. asfsdfadf', 'sdfasdfasdf sdfasdfasdf', 2),
-    createItem('04. asfsdfadf', 'sdfasdfasdf sdfasdfasdf ', 1),
-    createItem('02. asfsdfadf', 'sdfasdfasdf sdfasdfasdf 456456456 ghdfhgdfg sasdfasf vbmnXCdfg 678k', 5),
-    createItem('03. asfsdfadf', 'sdfasdfasdf sdfasdfasdf 456456456 ghdfhgdfg sasdfasf vbmnXCdfg 678k', 3),
-    createItem('04. asfsdfadf', 'sdfasdfasdf sdfasdfasdf ', 1),
-    createItem('05. asfsdfadf', 'sdfasdfasdf sdfasdfasdf', 2),
-    createItem('06. asfsdfadf', 'sdfasdfasdf sdfasdfasdf 456456456 ghdfhgdfg sasdfasf vbmnXCdfg 678k', 5),
-    createItem('07. asfsdfadf', 'sdfasdfasdf sdfasdfasdf 456456456 ghdfhgdfg sasdfasf vbmnXCdfg 678k', 3),
-    createItem('08. asfsdfadf', 'sdfasdfasdf sdfasdfasdf ', 1),
-    createItem('09. asfsdfadf', 'sdfasdfasdf sdfasdfasdf', 2),
-    createItem('10. asfsdfadf', 'sdfasdfasdf sdfasdfasdf 456456456 ghdfhgdfg sasdfasf vbmnXCdfg 678k', 5),
-    createItem('11. asfsdfadf', 'sdfasdfasdf sdfasdfasdf 456456456 ghdfhgdfg sasdfasf vbmnXCdfg 678k', 3),
-    createItem('12. asfsdfadf', 'sdfasdfasdf sdfasdfasdf', 2)
-];
+    return module;
 
-var collectionViewer = new CollectionViewer("#main", 
-                                        "#view-container .collection-view .list-container", 
-                                        collectionItems, 
-                                        "my-collection");
+})(window, jQuery);
 
-ko.applyBindings(collectionViewer);
-
-collectionViewer.show(0);
