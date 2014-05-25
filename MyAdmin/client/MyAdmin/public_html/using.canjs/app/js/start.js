464d35c79f53bf9efa3608a8a8010d334530abbe
freesrc <test@example.com>
2014-05-25 16:29:47 +0300

    My WebAPI admin

---
+++ MyAdmin/client/MyAdmin/public_html/using.canjs/app/js/start.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+define(['jquery','underscore', 'bootstrap'], function($) {
+    console.log();
+    $(document).ready(function() {
+       console.log(); 
+    });
+    
+    var app = {
+      start: function() {
+          return 0;
+      }
+    };
+    return app;
+});
