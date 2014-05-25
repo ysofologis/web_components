5a773cc83030fe14caa159eb03e892b50c9f622c
freesrc <test@example.com>
2014-05-25 18:50:41 +0300

    see last commit

---
+++ MyAdmin/server/MyAPI/myapi/api/User.py
@@ -0,0 +1,17 @@
+'''
+Created on May 25, 2014
+
+@author: freesrc
+'''
+
+class User(object):
+    '''
+    classdocs
+    '''
+    
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
+        
