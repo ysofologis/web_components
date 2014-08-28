27a914236c5f29b638d1b740383ad3f9fad5b640
freesrc <freesrc@localhost.localdomain>
2014-08-28 12:42:08 +0300

    [-] test more than one service points inside a test suite [-] add custom failure messages

---
+++ BehavioralTests/public_html/lib/jasmine-1.3.1/jasmine-custom-message.js
@@ -0,0 +1,77 @@
+/*
+ * jasmine-custom-message
+ * https://github.com/avrelian/jasmine-custom-message
+ *
+ * Copyright (c) 2014 Sergey Radchenko
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+(function() {
+  var global = Function('return this')();
+  if (! (global.jasmine && global.expect)) {
+    return;
+  }
+
+  var isBrowserEnv = global.window && global === global.window;
+  var isCommonJS = typeof module !== 'undefined' && module.exports;
+
+
+  var ofType = function(val) {
+    var types = [].slice.call(arguments, 1);
+    var valType = val === null ? 'null' : typeof val;
+    return types.indexOf(valType) > -1;
+  };
+
+  var getMessage = function(assertion, message, args) {
+    while (! ofType(message, 'string', 'number', 'boolean')) {
+      switch (true) {
+        case ofType(message, 'undefined', 'null'):
+          message = 'You cannot use `' + message + '` as a custom message';
+          break;
+        case ofType(message, 'function'):
+          message = message.apply(assertion, args);
+          break;
+        case message && ofType(message.toString, 'function') && message.toString().indexOf('[object ') !== 0:
+          message = message.toString();
+          break;
+        case JSON && ofType(JSON.stringify, 'function'):
+          message = JSON.stringify(message);
+          break;
+        default:
+          message = 'N/A';
+      }
+    }
+
+    return message.toString();
+  };
+
+  var wrapExpect = function(expect, customMessage) {
+    return function(actual) {
+      var assertion = expect(actual);
+      if (! ofType(customMessage, 'undefined', 'null')) {
+        assertion.message = function() {
+          return getMessage(assertion, customMessage, arguments);
+        };
+      }
+      return assertion;
+    };
+  };
+
+  var defineSince = function() {
+    global.since = function(customMessage) {
+      return {
+        expect: wrapExpect(global.expect, customMessage)
+      };
+    };
+  };
+
+  if (isBrowserEnv) {
+    defineSince();
+  } else {
+    if (isCommonJS) {
+      module.exports = defineSince();
+    }
+  }
+})();
--- BehavioralTests/public_html/spec/ProductInfo.js
+++ BehavioralTests/public_html/spec/ProductInfo.js
@@ -4,33 +4,39 @@
  * and open the template in the editor.
  */
 
-+function(config, expect) {
++function(config, since) {
     describe("Product Information", function() {
         var services = config.get('services');
         var resources = config.get('resources');
         for (var ix = 0; ix < services.length; ix++) {
             var serviceConf = services[ix];
             var apiUrl = serviceConf.baseUrl + resources.productinfo.path;
-            it("should return product info from [" + apiUrl + "]", function() {
-                var productInfo = null;
-                var req = new AjaxRequest({
-                    url: apiUrl,
-                    success: function(data) {
-                        productInfo = data;
-                    }
+            // protect closue references inside loop
+            +function(apiUrl) {
+                it("should return product info from [" + apiUrl + "]", function() {
+                    var productInfo = null;
+                    var req = new AjaxRequest({
+                        url: apiUrl,
+                        success: function(data) {
+                            productInfo = data;
+                        }
+                    });
+                    runs(function() {
+                        req.execute();
+                    });
+                    waitsFor(function() {
+                        return req.isCompleted();
+                    }, "request completed", 2500);
+                    runs(function() {
+                        since("http status must be 200").
+                                expect(req.status()).toEqual(200);
+                        since("product info should contain data").
+                                expect(productInfo).not.toBe(null);
+                    });
                 });
-                runs(function() {
-                    req.execute();
-                });
-                waitsFor(function() {
-                    return req.isCompleted();
-                }, "request completed", 2500);
-                runs(function() {
-                    expect(req.status()).equalTo(202);
-                    expect(productInfo).not.toBe(null);
-                });
-            });
+            }(apiUrl);
+
         }
     });
-}(appConfig, expect);
+}(appConfig, since);
 
